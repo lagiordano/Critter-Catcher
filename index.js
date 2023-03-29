886d038f06424a894de19f5ea9fbb366969a0eca
Lauren Giordano <laurengiordano94@gmail.com>
2023-03-30 10:19:43 +1100

    Finished responsive design layout

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function initialize () {
     const ul = document.querySelector("#creature-list");
     const creatureButtons = document.querySelector("#creature-buttons");
     const dropDown = document.querySelector("#months");
-    let newList = document.querySelector("#filtered-creature-list");
-    let categoryObject = {};
+    // let categoryObject = {};
     
 
     // Create list by category
@@ -54,21 +53,22 @@ function initialize () {
             
 
             let details = document.createElement("details");
-            details.className = "collapsed";                
+            details.classList.add("collapsed", "list-group-item", "col-12", "col-sm-11", "col-md-9", "col-lg-8", "col-xl-7");  
             details.innerHTML = populateInfoCard(creatureObject);
             ul.appendChild(details);
 
             // Toggle event listener to change styling when details are open
             details.addEventListener("toggle", () => {
-                if (details.className === "collapsed") {
-                    details.className = "expanded";
+                if (details.classList.contains("collapsed")) {
+                    details.classList.replace("collapsed", "expanded");
                 } else {
-                    details.className = "collapsed";
+                    details.classList.replace("expanded", "collapsed");
                 };
             });
 
             // Wish list 
             details.addEventListener("click", (e) => {
+
                 wishListGenerator(e, details);
             });
 
@@ -83,10 +83,10 @@ function initialize () {
         if (creatureObject["availability"]["time"] === "") {
             timeAvailable = "I'm around all day";
         } else {
-            timeAvailable =  `I'm only around from ${creatureObject["availability"]["time"]}`;
+            timeAvailable =  `${creatureObject["availability"]["time"]}`;
         };
         if (creatureObject["availability"]["month-array-southern"].length === 12) {
-            monthAvailable = "You can find me any month of the year";
+            monthAvailable = "12 Months of the year!";
         } else {
             let monthArray = creatureObject["availability"]["month-array-southern"];
             let newArr = monthArray.map( (num) => {
@@ -121,13 +121,23 @@ function initialize () {
             monthAvailable = newArr.toString();
         };
         return `
-        <summary class="summary">${creatureObject["name"]["name-EUen"]}</summary>
-        <img src="${creatureObject["icon_uri"]}">
-        <p>${creatureObject["catch-phrase"]}</p>
-        <p>Time available: ${timeAvailable}</p>
-        <p>Months available: ${monthAvailable}</p>
-        <p>Sale price: $${creatureObject["price"]}</p>
-        <button class="addInfo">Add to your To-Catch list</button>
+        <summary class="summary text-capitalize pb-2">${creatureObject["name"]["name-EUen"]}</summary>
+            <div class=" card-deck d-flex justify-content-center">
+                <div id="card-sizes" class="card">
+                    <div class="card-img-top d-flex justify-content-center">
+                        <img src="${creatureObject["icon_uri"]}">
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title text-center">${creatureObject["name"]["name-EUen"].toUpperCase()}</h4>
+                        <p class="card-text text-center">Months: ${monthAvailable}</p>
+                        <p class="card-text text-center">Times: ${timeAvailable}</p>
+                        <p class="card-text text-center">Sells for: $${creatureObject["price"]}</p>
+                        <div class="d-flex justify-content-center">
+                            <button class=" btn btn-outline-success align-items-center addInfo" >Add to Catch-List</button>
+                        </div>
+                    </div>
+                </div>
+            </div>        
         `
     };
 
@@ -137,9 +147,7 @@ function initialize () {
     function filterList (categoryObject, selected) {
         let filtered = {};
         for (let item in categoryObject) {
-            // console.log(item);
             creatureObject = {...categoryObject[item]};
-            // console.log(creatureObject);
             let monthArray = creatureObject["availability"]["month-array-southern"];
             let monthString = monthArray.toString();
             if(monthString.indexOf(selected) !== -1) {
@@ -153,25 +161,23 @@ function initialize () {
     }
 
 
-
-    
-
-
-    
-
-
     //Add and remove form wish list
     function wishListGenerator(e, details) {
         if (e.target.tagName === "BUTTON") {
-            let div = document.createElement("div");
             let findContainer = document.querySelector("#find-container");
-            div.className = "wishListCard";
-            findContainer.appendChild(div);
-            div.innerHTML = details.innerHTML;
-            let addInfo = div.querySelector(".addInfo");
-            addInfo.textContent = "Found!";
-            addInfo.addEventListener("click", () => {
-                addInfo.parentNode.remove();
+            
+            findContainer.classList.add("card-deck");
+            let catchCard = document.createElement("div");
+            catchCard.innerHTML = details.innerHTML;
+            catchCard.classList.add("card", "col-12", "col-sm-11", "col-md-6", "col-lg-4")
+            catchCard.removeChild(catchCard.firstElementChild);
+            findContainer.appendChild(catchCard);
+            catchCard.querySelector("#card-sizes").classList.remove("card", "col-12", "col-sm-11", "col-md-9", "col-lg-8", "col-xl-7");
+            catchCard.querySelector("button").classList.replace("addInfo", "found-btn");
+            let foundButton = catchCard.querySelector(".found-btn");
+            foundButton.textContent = "Found!";
+            foundButton.addEventListener("click", () => {
+                catchCard.remove();
             });
         };
     };
