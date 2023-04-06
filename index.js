db7cb7088d1f04c58e26d76435d7176046af2f85
Lauren Giordano <laurengiordano94@gmail.com>
2023-04-06 11:07:23 +1000

    majority of review changes implemented

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function initialize () {
     const ul = document.querySelector("#creature-list");
     const creatureButtons = document.querySelector("#creature-buttons");
     const dropDown = document.querySelector("#months");
-    let errorMsg = document.querySelector("#error-msg");
+    
     
     
     
@@ -14,26 +14,38 @@ function initialize () {
     // Create list by category
     creatureButtons.addEventListener("click", e => {
         if (e.target.tagName === "BUTTON") {
-            errorMsg.classList.replace("d-block", "d-none");
             ul.textContent = "";
             let type = e.target.textContent;
             initiateList(type);
 
         };
+
+        let btnGroup = e.target.closest('.btn-group');
+        let siblings = btnGroup.querySelectorAll("button");
+        for (let sibling of siblings) {
+            if (sibling === e.target) {
+                sibling.classList.add("btn-selected");
+                console.log("selected");
+            } else {
+                sibling.classList.remove("btn-selected");
+                console.log("not selected");
+            }
+        }
+
+
         document.querySelector("form").reset();
     });
     
 
     // Fetch data and calls populateList function 
-    function initiateList (creature) {
-        let type = creature.toLowerCase();
+    function initiateList (type) {
+        type = type.toLowerCase();
         fetch(`https://acnhapi.com/v1/${type}`)
         .then(res => res.json())
         .then(creatureData => {
             let categoryObject = {...creatureData};
 
-           
-
+        
             populateList(categoryObject);
 
 
@@ -46,8 +58,7 @@ function initialize () {
 
         })
         .catch( () => {
-            errorMsg.classList.add("d-block")
-            errorMsg.textContent = "Sorry, there's been an error gathering up those creatures for you.";
+            alert("Sorry, there's been an issue loading those creatures!");
         });
         
         
@@ -56,14 +67,52 @@ function initialize () {
     
     // Populates list
     function populateList (categoryObject) {
+        // console.log(categoryObject);
         for (let item in categoryObject) {
-            creatureObject = {...categoryObject[item]};
-            
+            let creatureObject = {...categoryObject[item]};
+
+            // console.log(creatureObject);
+
+            let creatureInfo = generateObject(creatureObject);
+
+            // console.log(creatureInfo);
 
             let details = document.createElement("details");
             details.classList.add("collapsed", "list-group-item", "col-12", "col-sm-11", "col-md-9", "col-lg-8", "col-xl-7");  
-            details.innerHTML = populateInfoCard(creatureObject);
+            
+            let summary = document.createElement("summary");
+            summary.textContent = creatureInfo.name;
+            summary.classList.add("text-capitalize", "pb-2");
+
+            let cardDiv = document.createElement("div");
+            cardDiv.classList.add("card-deck", "d-flex", "justify-content-center");
+            cardDiv.innerHTML = renderCard(creatureInfo)
+
+
+            details.appendChild(summary);
+            details.appendChild(cardDiv);
             ul.appendChild(details);
+
+            // Add buttons to each creature <details>
+            let buttonDiv = document.createElement("div");
+            let addListBtn = document.createElement("button");
+            let goToBtn = document.createElement("a");
+            buttonDiv.appendChild(addListBtn);
+            buttonDiv.appendChild(goToBtn);
+            details.appendChild(buttonDiv);
+
+            buttonDiv.classList.add("d-flex", "justify-content-center", "p-3");
+            addListBtn.textContent = "Add To List";
+            addListBtn.classList.add("btn", "btn-outline-success", "align-items-center", "mx-1", "mx-md-2", "mx-lg-3")
+            addListBtn.id = "add-card-btn";
+            goToBtn.setAttribute("href", "#heading1");
+            goToBtn.textContent = "Go to List";
+            goToBtn.classList.add("btn", "btn-success", "align-items-center", "mx-1", "mx-md-2", "mx-lg-3");
+            
+
+            // let card = querySelector("#grab-card");
+
+            // console.log(card);
 
             // Toggle event listener to change styling when details are open
             details.addEventListener("toggle", () => {
@@ -74,9 +123,10 @@ function initialize () {
                 };
             });
 
-            // Wish list 
+            // // Wish list 
+
             details.addEventListener("click", (e) => {
-                wishListGenerator(e, details);
+                wishListGenerator(e, creatureInfo);
             });
 
         };
@@ -84,7 +134,9 @@ function initialize () {
 
 
 
-    function populateInfoCard (creatureObject) {
+    function generateObject (creatureObject) {
+
+        // Extract date and time data into readable info
         let timeAvailable;
         let monthAvailable;
         if (creatureObject["availability"]["time"] === "") {
@@ -126,29 +178,94 @@ function initialize () {
                 };
             });
             monthAvailable = newArr.toString();
+            
         };
-        return `
-        <summary class="summary text-capitalize pb-2">${creatureObject["name"]["name-EUen"]}</summary>
-            <div class=" card-deck d-flex justify-content-center">
-                <div id="card-sizes" class="card col-11 col-sm-10 col-md-9 col-lg-8 col-xl-7">
-                    <div class="card-img-top d-flex justify-content-center">
-                        <img src="${creatureObject["icon_uri"]}">
-                    </div>
-                    <div class="card-body">
-                        <h4 class="card-title text-center">${creatureObject["name"]["name-EUen"].toUpperCase()}</h4>
-                        <p class="card-text text-center">Months: ${monthAvailable}</p>
-                        <p class="card-text text-center">Times: ${timeAvailable}</p>
-                        <p class="card-text text-center">Sells for: $${creatureObject["price"]}</p>
-                        <div class="d-flex justify-content-center">
-                            <button class="btn btn-outline-success align-items-center addInfo mx-1 mx-md-2 mx-lg-3 list-btn" >Add to List</button>
-                            <button class="btn btn-success align-items-center mx-md-2 mx-lg-3 to-list"><a class="text-decoration-none text-white" href="#heading1">Go to List</a></button>
-                        </div>
-                    </div>
-                </div>
-            </div>        
-        `
-        "col-12", "col-sm-11", "col-md-9", "col-lg-8", "col-xl-7"
+
+        // create object holding creature data for card
+
+        let creatureInfo = {
+            name: creatureObject["name"]["name-EUen"],
+            imageURL: creatureObject["icon_uri"],
+            price: creatureObject["price"],
+            month: monthAvailable,
+            time: timeAvailable
+        };
+
+        return creatureInfo;
+
     };
+
+
+
+        function renderCard (creatureInfo) {
+
+            
+
+            const returnDiv = document.createElement("div");
+            const div = document.createElement("div");
+            const cardBody = document.createElement("div");
+            const image = document.createElement("img");
+            const name = document.createElement("h4");
+            const p1 = document.createElement("p");
+            const p2 = document.createElement("p");
+            const p3 = document.createElement("p");
+
+            
+            div.classList.add("card", "col-11", "col-sm-10", "col-md-9", "col-lg-8", "col-xl-7", "align-items-center");
+            image.classList.add("card-img-top");
+            cardBody.classList.add("card-body");
+            
+            name.classList.add("card-title", "text-center");
+            p1.classList.add("card-text", "text-center");
+            p2.classList.add("card-text", "text-center");
+            p3.classList.add("card-text", "text-center");
+
+
+            image.setAttribute("src", creatureInfo.imageURL);
+            image.style.width = "50%";
+            name.innerText = creatureInfo.name.toUpperCase();
+            p1.innerText = `Months: ${creatureInfo.month}`;
+            p2.innerText = `Times: ${creatureInfo.time}`;
+            p3.innerText = `Sells for: $${creatureInfo.price}`
+
+            
+            cardBody.appendChild(name);
+            cardBody.appendChild(p1);
+            cardBody.appendChild(p2);
+            cardBody.appendChild(p3);
+            div.appendChild(image);
+            div.appendChild(cardBody);
+            returnDiv.appendChild(div);
+
+            return returnDiv.innerHTML;
+            
+        };
+
+
+       
+
+    //     return `
+    //     <summary class="summary text-capitalize pb-2">${creatureObject["name"]["name-EUen"]}</summary>
+    //         <div class=" card-deck d-flex justify-content-center">
+    //             <div id="card-sizes" class="card col-11 col-sm-10 col-md-9 col-lg-8 col-xl-7">
+    //                 <div class="card-img-top d-flex justify-content-center">
+    //                     <img src="${creatureObject["icon_uri"]}">
+    //                 </div>
+    //                 <div class="card-body">
+    //                     <h4 class="card-title text-center">${creatureObject["name"]["name-EUen"].toUpperCase()}</h4>
+    //                     <p class="card-text text-center">Months: ${monthAvailable}</p>
+    //                     <p class="card-text text-center">Times: ${timeAvailable}</p>
+    //                     <p class="card-text text-center">Sells for: $${creatureObject["price"]}</p>
+    //                     <div class="d-flex justify-content-center">
+    //                         <button class="btn btn-outline-success align-items-center addInfo mx-1 mx-md-2 mx-lg-3 list-btn" >Add to List</button>
+    //                         <button class="btn btn-success align-items-center mx-md-2 mx-lg-3 to-list"><a class="text-decoration-none text-white" href="#heading1">Go to List</a></button>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>        
+    //     `
+    //     "col-12", "col-sm-11", "col-md-9", "col-lg-8", "col-xl-7"
+    // };
 
 
 
@@ -168,25 +285,60 @@ function initialize () {
 
 
     //Add and remove form wish list
-    function wishListGenerator(e, details) {
-        if (e.target.classList.contains("list-btn")) {
+    function wishListGenerator(e, creatureInfo) {
+        
+
+        if (e.target.id === "add-card-btn") {
+            let addBtn = e.target;
+            addBtn.disabled = true;
+
+            document.querySelector("#empty-list").style.display = "none";
+
             let findContainer = document.querySelector("#find-container");
-            findContainer.querySelector("p").style.display = "none";
-            findContainer.classList.add("card-deck");
-            let catchCard = document.createElement("div");
-            catchCard.innerHTML = details.innerHTML;
-            catchCard.classList.add("card", "col-10", "col-md-6", "col-lg-4");
-            catchCard.removeChild(catchCard.firstElementChild);
-            findContainer.appendChild(catchCard);
-            catchCard.querySelector("#card-sizes").classList.remove("card", "col-11", "col-sm-10", "col-md-9", "col-lg-8", "col-xl-7");
-            catchCard.querySelector(".to-list").remove();
-            catchCard.querySelector(".list-btn").classList.replace("addInfo", "found-btn");
-            let foundButton = catchCard.querySelector(".found-btn");
-            foundButton.textContent = "Found!";
-            foundButton.addEventListener("click", () => {
-                catchCard.remove();
-            });
-        };
+            
+            // findContainer.classList.add("card-deck");
+
+            let div = document.createElement("div");
+            div.classList.add("col", "d-flex", "justify-content-center");
+            div.innerHTML = renderCard(creatureInfo);
+            console.log(div);
+            let card = div.querySelector(".card");
+            card.classList.remove("col-sm-10", "col-md-9", "col-lg-8", "col-xl-7");
+            card.classList.add("mb-3", "mb-md-4", "mb-lg-5");
+            findContainer.appendChild(div);
+
+
+            let foundDiv = document.createElement("div");
+            let foundBtn = document.createElement("button");
+            foundDiv.classList.add("d-flex", "justify-content-center", "p-3");
+            foundBtn.classList.add("btn", "btn-outline-success", "align-items-center", "mx-1", "mx-md-2", "mx-lg-3");
+            foundBtn.textContent = "Found!";
+            foundDiv.appendChild(foundBtn);
+            card.appendChild(foundDiv);
+            
+            foundBtn.addEventListener("click", () => {
+                div.remove();
+                addBtn.disabled = false;
+            })
+            
+            
+        }
+
+        // if (e.target.classList.contains("list-btn")) {
+        //    
+        //     catchCard.classList.add("card", "col-10", "col-md-6", "col-lg-4");
+        //     catchCard.removeChild(catchCard.firstElementChild);
+        //     findContainer.appendChild(catchCard);
+        //     catchCard.querySelector("#card-sizes").classList.remove("card", "col-11", "col-sm-10", "col-md-9", "col-lg-8", "col-xl-7");
+        //     catchCard.querySelector(".to-list").remove();
+        //     catchCard.querySelector(".list-btn").classList.replace("addInfo", "found-btn");
+        //     let foundButton = catchCard.querySelector(".found-btn");
+        //     foundButton.textContent = "Found!";
+        //     foundButton.addEventListener("click", () => {
+        //         catchCard.remove();
+        //         e.target.disabled = false;
+        //     });
+        // };
     };
 
 
